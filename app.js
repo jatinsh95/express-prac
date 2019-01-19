6d8d9a5a200c2124c344948ad0194bc1d9a9801b
jatin sharma <jatinsh95@users.noreply.github.com>
2019-01-19 18:31:05 +0530

    mywork

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const lC = require('./listCommunication');
 const app = express();
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 app.get('/api/customer/:id', (req, res) => {
-    lC.GetCustomer(req.params.id).then((data)=>{
+    lC.GetCustomer(req.params.id).then((data) => {
         res.send(data);
-    },(err)=>{
+    }, (err) => {
         res.send(err);
     });
 });
 app.get('/api/customer/', (req, res) => {
-    lC.GetCustomer().then((data)=>{
+    lC.GetCustomer().then((data) => {
         res.send(data);
-    },(err)=>{
+    }, (err) => {
         res.send(err);
     });
+});
+
+app.post('/api/customer',(req,res)=>{
+    res.send(req.body);
+    // lC.CreateCustomer(JSON.parse(req.body)).then((data)=>{
+    //     res.send(data);
+    // },(err)=>{
+    //     res.send(err);
+    // });
 });
 app.listen(3000, () => {
     console.log('server is running');
--- listCommunication.js
+++ listCommunication.js
@@ -1,32 +1,56 @@
-// "use strict"
+"use strict"
 const fs = require('fs');
 
 var GetCustomer = GetCustomer;
+var CreateCustomer = CreateCustomer;
 function GetCustomer(id) {
-return  new Promise((resolve,reject)=>{
-    if (id) {
-        fs.readFile('customerList.json', (err, data) => {
-            if (err) {
-                reject(err);
-            }
-            let list = JSON.parse(data);
-            resolve(JSON.stringify(list[id]));
-        });
-    } else {
-        fs.readFile('customerList.json', (err, data) => {
-            if (err) {
-                reject(err);
-            }
-            let list = JSON.parse(data);
-            resolve(JSON.stringify(list));
-        });
-    }
-});
-
+    return new Promise((resolve, reject) => {
+        if (id) {
+            fs.readFile('customerList.json', (err, data) => {
+                if (err) {
+                    reject(err);
+                }
+                let list = JSON.parse(data);
+                resolve(JSON.stringify(list[id]));
+            });
+        } else {
+            fs.readFile('customerList.json', (err, data) => {
+                if (err) {
+                    reject(err);
+                }
+                let list = JSON.parse(data);
+                resolve(JSON.stringify(list));
+            });
+        }
+    });
 
     return customer;
 }
 
+function CreateCustomer(obj) {
+    return new Promise((resolve, reject) => {
+        if (typeof obj === 'object') {
+            fs.readFile('customerList.json', (err, data) => {
+                if (err) {
+                    reject(err);
+                }
+                let list = JSON.parse(data);
+                obj['id'] = list[list.length - 1].id + 1;
+                list.push(obj);
+                fs.writeFile('customerList.json', JSON.stringify(list),(err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(list);
+                    }
+                })
+            });
+        }
+    })
+
+}
+
 module.exports = {
-    GetCustomer
+    GetCustomer,
+    CreateCustomer
 };
